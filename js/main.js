19bd2dbeb6307ce00a3deff8e39e1608a68ff3d2
sofiabeehere <sofiambautista@gmail.com>
2017-12-06 13:24:48 -0800

    Updated browse.html and styling for the same page through _base.scss, _sidebar.scss. Updated animations for browse.html through main.js and tabs.js.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+// Parallax Images
+
 $('.parallax-window').parallax({imageSrc: 'img/banner.png'});
 
 $('.env-parallax-window').parallax({imageSrc: 'img/programs/envsci.png'});
+
+// Dropdowns in Filtering Sidebar for browse.html
 
 $(function() {
     function toggleDropdown(t) {
@@ -9,3 +13,39 @@ $(function() {
         $(t).toggleClass('hide-border');
     }
 });
+
+// Reading progress indicator for details.html
+
+$('.progress-sidebar a').on('click', function() {
+
+	var scrollAnchor = $(this).attr('data-scroll'),
+	scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 75;
+
+	$('body,html').animate({
+	scrollTop: scrollPoint
+	}, 500);
+
+	return false;
+
+})
+
+
+$(window).scroll(function() {
+	var windscroll = $(window).scrollTop();
+	if (windscroll >= 100) {
+		$('.progress-sidebar').addClass('fixed');
+		$('.main-program-content section').each(function(i) {
+		    if ($(this).position().top <= windscroll - 20) {
+		        $('.progress-sidebar a.active').removeClass('active');
+		        $('.progress-sidebar a').eq(i).addClass('active');
+		    }
+		});
+
+	} else {
+
+		$('.progress-sidebar').removeClass('fixed');
+		$('.progress-sidebar a.active').removeClass('active');
+		$('.progress-sidebar a:first').addClass('active');
+	}
+
+}).scroll();
--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,16 @@
-function openYear(evt, yearName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(yearName).style.display = "flex";
-	evt.currentTarget.className += " active";
-}
+// function openYear(evt, yearName) {
+// 	var i, tabcontent, tablinks;
+// 	tabcontent = document.getElementsByClassName("tabcontent");
+// 	for (i = 0; i < tabcontent.length; i++) {
+// 		tabcontent[i].style.display = "none";
+// 	}
+// 	tablinks = document.getElementsByClassName("tablinks");
+// 	for (i = 0; i < tablinks.length; i++) {
+// 		tablinks[i].className = tablinks[i].className.replace(" active", "");
+// 	}
+// 	document.getElementById(yearName).style.display = "flex";
+// 	evt.currentTarget.className += " active";
+// }
 
 function openReq(evt, yearName) {
 	var i, tabcontent, tablinks;
@@ -27,5 +27,5 @@ function openReq(evt, yearName) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// document.getElementById("defaultOpen").click();
 document.getElementById("defaultReqOpen").click();
