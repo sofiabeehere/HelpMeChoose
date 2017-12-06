0da38773bb9f74d9f2d8c87340ff939876ab22c5
sofiabeehere <sofiambautista@gmail.com>
2017-12-05 22:39:30 -0800

    Temporary fix for featured programs in index.html. Updated styling in _base.scss, _header.scss, _sidebar.scss and content in details.html.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $('.parallax-window').parallax({imageSrc: 'img/banner.png'});
 
+$('.env-parallax-window').parallax({imageSrc: 'img/programs/envsci.png'});
+
 $(function() {
     function toggleDropdown(t) {
          $(t).siblings(".dropdown-content").toggle();
